501a3d3c22fab04547acce469d2fc6c7e8308a7a
Khelsai Rajwade <khelsai@gmail.com>
2023-11-02 18:48:02 +0530

    resume

--- khelsai_portfolio/src/data/constants.js
+++ khelsai_portfolio/src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/khelsai01",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/16NkjgreteOuo-HDSxBgTWZH_Dy5jAmOU/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/khelsai-rajwade-77a235258/",
   twitter: "",
   insta: "",
